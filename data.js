29e3b01f1ec428d128e935cdb3d4ee8e08bab0a7
Olga777930 <ok748681@gmail.com>
2025-04-07 11:45:18 +0200

    360

---
+++ data.js
@@ -0,0 +1,132 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--",
+      "name": "до ПЕРЕХОД",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "yaw": 0.5259303090721001,
+        "pitch": 0.028683373927307798,
+        "fov": 1.4719542186222143
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.14347128993365033,
+          "pitch": 0.16984790745368983,
+          "rotation": 0,
+          "target": "3--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1--jpeg",
+      "name": "ДО ПРИХОЖАЯjpeg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6390319388747585,
+          "pitch": 0.14096518541626324,
+          "rotation": 0,
+          "target": "3--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--",
+      "name": "до СПАЛЬНЯ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -2.584010212035883,
+        "pitch": 0.11217135440130477,
+        "fov": 1.4719542186222143
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.584010212035883,
+          "pitch": 0.11217135440130477,
+          "rotation": 0,
+          "target": "1--jpeg"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--",
+      "name": "ДО УЛИЦА",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
